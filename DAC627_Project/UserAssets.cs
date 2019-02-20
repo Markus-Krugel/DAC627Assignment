e68ca3d979f63f5cef75a93212c41043c8a7acb7
Mininod <35764343+Mininod@users.noreply.github.com>
2019-02-20 16:25:11 +0000

    user assets

---
+++ DAC627_Project/UserAssets.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DAC627_Project
+{
+    public class UserAssets
+    {
+        private string _assetName;
+        private UsersAccounts.UserData _author;
+        private List<string> _picturesPath;
+        private string _assetPath;
+
+        public string GetAssetName()
+        {
+            return _assetName;
+        }
+
+        public UsersAccounts.UserData GetAuthor()
+        {
+            return _author;
+        }
+
+        public List<string> GetPicturesPath()
+        {
+            return _picturesPath;
+        }
+
+        public string AssetPath() 
+        {
+            return _assetPath;
+        }
+    }
+}
